e0d46252627a2e6a0ee7ce82f802f724970b9ce5
Gourav Biswas <gourav.biswas11@tcs.com>
2020-02-10 18:23:57 +0100

    first commit

---
+++ src/main/java/com/app/mfu/mfuApp.java
@@ -0,0 +1,15 @@
+package com.app.mfu;
+
+public class mfuApp {
+	
+	private String technology="Devops";
+
+	public String getTechnology() {
+		return technology;
+	}
+
+	public void setTechnology(String technology) {
+		this.technology = technology;
+	}
+
+}
